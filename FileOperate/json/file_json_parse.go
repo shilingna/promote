00bdfb1d9395d1244d64acbc0f6426854a312814
shilingna <13723617615@163.com>
2022-06-08 23:57:45 +0800

    dream

--- FileOperate/json/file_json_parse.go
+++ FileOperate/json/file_json_parse.go
@@ -11,8 +11,8 @@ import (
 // 定义配置文件解析后的结构
 type (
 	OrderDetail struct {
-		OrderId       string `json:"order_id"`
-		ReceiveName   string `json:"receiveName"`
+		OrderId       string `json:"orderId,omitempty"`
+		ReceiveName   string `json:"receiveName,_"`
 		ReceiveMobile string `json:"receiveMobile"`
 		ReceiveAddr   string `json:"receiveAddr"`
 	}
@@ -24,11 +24,16 @@ type (
 
 func main() {
 	v := Order{}
-	date, err := ioutil.ReadFile("./json_parse.json")
+	date, err := ioutil.ReadFile("./FileOperate/json/json_parse.json")
 	if err != nil {
 		fmt.Println(err)
 	}
 	err = json.Unmarshal(date, &v)
 	json, _ := json.Marshal(v)
-	fmt.Println()
+	fmt.Println("JSON data is :", string(json))
+	fmt.Println("OrderNo is :", v.OrderNo)
+	fmt.Println("OrderId is :", v.OrderDetail.OrderId)
+	fmt.Println("ReceiveName is :", v.OrderDetail.ReceiveName)
+	fmt.Println("ReceiveAdder is :", v.OrderDetail.ReceiveAddr)
+	fmt.Println("ReceiveMobile is :", v.OrderDetail.ReceiveMobile)
 }
